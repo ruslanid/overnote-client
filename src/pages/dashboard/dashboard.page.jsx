331e053a132cd4d32322c9bb53ef3d6c916cf57a
unknown <ruslan.idrisov01@gmail.com>
2020-08-05 20:11:56 -0400

    Initialize react app. Add dashboard page.

---
+++ src/pages/dashboard/dashboard.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {
+
+} from './dashboard.styles';
+
+const DashboardPage = () => (
+  <div className="dashboard-page">
+    Dashboard
+  </div>
+);
+
+export default DashboardPage;
