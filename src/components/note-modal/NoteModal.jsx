49c7935b2ca6837ff4133af6f94f0bed96a8cfe8
iMears <maxwellmears@gmail.com>
2020-08-18 21:07:38 -0700

    Add note modal form placeholder

--- src/components/note-modal/NoteModal.jsx
+++ src/components/note-modal/NoteModal.jsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { Modal } from "semantic-ui-react";
+import { Button, Modal } from "semantic-ui-react";
+import NoteForm from "./NoteForm";
 
-function NoteModal({ trigger, note: { title, description } }) {
-  return <Modal trigger={trigger} header={title} content={description} />;
-}
+const saveNote = () => {};
+const deleteNote = () => {};
+
+const NoteModal = ({ trigger, note, note: { title } }) => (
+  <Modal trigger={trigger}>
+    <Modal.Header>{title}</Modal.Header>
+    <Modal.Content>
+      <Modal.Description>
+        <NoteForm note={note} />
+      </Modal.Description>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button onClick={() => deleteNote(false)} color="red">
+        Delete
+      </Button>
+      <Button onClick={() => saveNote(false)} color="blue">
+        Save
+      </Button>
+    </Modal.Actions>
+  </Modal>
+);
 
 export default NoteModal;
